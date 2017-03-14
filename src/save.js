9b2e13cb4d2744d0a6056f281c23bf1337798a74
farawayer <farwayer@gmail.com>
2017-03-15 01:40:13 +0530

    save

---
+++ src/save.js
@@ -0,0 +1,140 @@
+import {runInAction} from 'mobx'
+import observe from './observe'
+import {invokedWithArgs, decorate} from './utils'
+
+
+const Status = {
+  NotInitialized: undefined,
+  Loading: 1,
+  SettingLoadedValue: 2,
+  Initialized: 3,
+};
+
+
+export default function save({
+  storage = defaultStorage(),
+  storeName,
+  transform = item => item,
+  onLoaded = () => {},
+  onSaved = () => {},
+  onInitialized = () => {},
+} = {}) {
+  const withArgs = invokedWithArgs(arguments);
+
+  function decorator(target, property, description) {
+    let status = Status.NotInitialized;
+
+    return observe(async function ({newValue: value}) {
+      const store = this;
+      const key = storageKey(store, storeName, property);
+
+      switch (status) {
+        case Status.NotInitialized: {
+          status = Status.Loading;
+
+          value = await loadProperty(storage, key, transform);
+
+          // check value was loaded and property was not modified by user
+          if (value !== undefined && status === Status.Loading) {
+            status = Status.SettingLoadedValue;
+
+            runInAction(`loading ${key} from storage`, () => {
+              store[property] = value;
+            });
+
+            onLoaded(store, property, value);
+          }
+
+          onInitialized(store, property, value);
+          break;
+        }
+
+        case Status.SettingLoadedValue: {
+          status = Status.Initialized;
+          break;
+        }
+
+        // property was modified by user while loading
+        case Status.Loading: {
+          status = Status.Initialized;
+
+          const saved = await saveProperty(storage, key, value);
+          if (!saved) break;
+
+          onSaved(store, property, value);
+          break;
+        }
+
+        case Status.Initialized: {
+          const saved = await saveProperty(storage, key, value);
+          if (!saved) break;
+
+          onSaved(store, property, value);
+          break;
+        }
+      }
+    }, true)(target, property, description);
+  }
+
+  return decorate(withArgs, decorator, arguments);
+}
+
+async function loadProperty(storage, key, transform) {
+  const json = await storage.getItem(key);
+  if (!json) return;
+
+  return transform(JSON.parse(json));
+}
+
+async function saveProperty(storage, key, value) {
+  const json = JSON.stringify(value);
+  if (!json) return;
+
+  await storage.setItem(key, json);
+  return true;
+}
+
+function storageKey(store, storeName=store.storeName, property) {
+  if (!storeName) {
+    throw new Error(
+      "You must define `storeName` property in store or pass it as option to " +
+      "@save"
+    );
+  }
+
+  return `${storeName}:${property}`;
+}
+
+function defaultStorage() {
+  if (typeof navigator === 'undefined') {
+    return MemoryStorage;
+  }
+
+  switch (navigator.product) {
+    case 'ReactNative':
+      return require('react-native').AsyncStorage;
+
+    default:
+      return AsyncLocalStorage;
+  }
+}
+
+export class AsyncLocalStorage {
+  static async getItem(key) {
+    return localStorage.getItem(key);
+  }
+  static async setItem(key, value) {
+    return localStorage.setItem(key, value);
+  }
+}
+
+export class MemoryStorage {
+  static db = {};
+
+  static async getItem(key) {
+    return MemoryStorage.db[key];
+  }
+  static async setItem(key, value) {
+    return MemoryStorage.db[key] = value;
+  }
+}
---
+++ test/save.js
@@ -0,0 +1,166 @@
+import {observable, action} from 'mobx'
+import {save} from '../'
+
+
+class Storage {
+  db = {};
+
+  constructor(state) {
+    this.db = Object.assign(this.db, state);
+  }
+
+  async getItem(key) {
+    return this.db[key];
+  }
+  async setItem(key, value) {
+    this.db[key] = value;
+  }
+}
+
+
+describe('save', () => {
+  it('should load value from storage', done => {
+    const storage = new Storage({
+      'user:loginCount': JSON.stringify(999),
+    });
+
+    class User {
+      storeName = 'user';
+
+      @save({
+        storage,
+        onLoaded: (store, property, value) => {
+          value.should.be.equal(999);
+          store.loginCount.should.be.equal(999);
+          done();
+        }
+      })
+      @observable
+      loginCount = 0;
+    }
+
+    const user = new User();
+    user.loginCount.should.be.equal(0);
+  });
+
+
+  it('should save value to storage', done => {
+    const storage = new Storage();
+
+    class User {
+      storeName = 'user';
+
+      @save({
+        storage,
+        onInitialized: store => {
+          store.login();
+          store.loginCount.should.be.equal(1);
+        },
+        onSaved: async (store, property, value) => {
+          value.should.be.equal(1);
+          store.loginCount.should.be.equal(1);
+          const saved = await storage.getItem('user:loginCount');
+          saved.should.be.equal(JSON.stringify(1));
+          done();
+        }
+      })
+      @observable
+      loginCount = 0;
+
+      @action login() {
+        this.loginCount += 1;
+      }
+    }
+
+    const user = new User();
+    user.loginCount.should.be.equal(0);
+  });
+
+
+  it('should be possible to specify/override storeName by options', done => {
+    const storage = new Storage({
+      'myUser:loginCount': JSON.stringify(999),
+    });
+
+    class User {
+      storeName = 'user';
+
+      @save({
+        storage,
+        storeName: 'myUser',
+        onLoaded: (store, property, value) => {
+          value.should.be.equal(999);
+          store.loginCount.should.be.equal(999);
+          done();
+        }
+      })
+      @observable
+      loginCount = 0;
+    }
+
+    const user = new User();
+    user.loginCount.should.be.equal(0);
+  });
+
+
+  it('should set user value while loading', done => {
+    const storage = new Storage({
+      'user:loginCount': JSON.stringify(999),
+    });
+
+    class User {
+      storeName = 'user';
+
+      @save({
+        storage,
+        onSaved: async (store, property, value) => {
+          value.should.be.equal(1);
+          store.loginCount.should.be.equal(1);
+
+          const saved = await storage.getItem('user:loginCount');
+          saved.should.be.equal(JSON.stringify(1));
+          done();
+        }
+      })
+      @observable
+      loginCount = 0;
+
+      @action login() {
+        this.loginCount += 1;
+      }
+    }
+
+    const user = new User();
+    user.loginCount.should.be.equal(0);
+    user.login();
+    user.loginCount.should.be.equal(1);
+  });
+
+
+  it('should transform works', done => {
+    const storage = new Storage({
+      'user:lastLogin': JSON.stringify(new Date(2013, 6, 17)),
+    });
+
+    class User {
+      storeName = 'user';
+
+      @save({
+        storage,
+        transform: value => new Date(value),
+        onLoaded: (store, property, value) => {
+          value.should.be.Date();
+          value.should.be.eql(new Date(2013, 6, 17));
+          store.lastLogin.should.be.Date();
+          store.lastLogin.should.be.eql(new Date(2013, 6, 17));
+          done();
+        }
+      })
+      @observable
+      lastLogin = new Date(2017, 3, 15);
+    }
+
+    const user = new User();
+    user.lastLogin.should.be.eql(new Date(2017, 3, 15));
+  });
+});
