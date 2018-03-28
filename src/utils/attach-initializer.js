85eb8a5ba33d725ca21e91b6a030c2f91e999940
farwayer <farwayer@gmail.com>
2018-03-29 00:52:56 +0300

    attachInitializer: do not stop spy target

--- src/utils/attach-initializer.js
+++ src/utils/attach-initializer.js
@@ -2,12 +2,11 @@ import {spy} from 'mobx'
 
 
 export default function attachInitializer(target, property, init) {
-  const stopSpy = spy(({type, object: store, key}) => {
+  spy(({type, object: store, key}) => {
     if (!target.isPrototypeOf(store)) return;
     if (type !== 'add') return;
     if (property && key !== property) return;
 
-    stopSpy();
-    init(store, property);
+    init(store);
   })
 }
