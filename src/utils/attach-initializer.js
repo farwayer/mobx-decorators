49d6ddbf2d11a445d055b2fb8feb5ebbfb9715e8
farwayer <farwayer@gmail.com>
2018-03-29 01:11:11 +0300

    attachInitializer: perf

--- src/utils/attach-initializer.js
+++ src/utils/attach-initializer.js
@@ -3,9 +3,9 @@ import {spy} from 'mobx'
 
 export default function attachInitializer(target, property, init) {
   spy(({type, object: store, key}) => {
-    if (!target.isPrototypeOf(store)) return;
     if (type !== 'add') return;
     if (property && key !== property) return;
+    if (!target.isPrototypeOf(store)) return;
 
     init(store);
   })
