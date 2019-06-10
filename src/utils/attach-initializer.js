c48d011a502d5c3058f0600cf3e63f001ef65197
farwayer <farwayer@gmail.com>
2019-06-10 10:36:21 +0300

    Revert "attach-initializer: warn mobx >4"
    This reverts commit 52d4bd59

--- src/utils/attach-initializer.js
+++ src/utils/attach-initializer.js
@@ -1,13 +1,7 @@
 import {spy} from 'mobx'
-import {version} from 'mobx/package'
 
 
 export default function attachInitializer(target, property, init) {
-  const major = version.split('.')[0];
-  if (major > 4) {
-    console.warn(`MobX >4 is not supported yet`)
-  }
-
   spy(({type, object: store, key}) => {
     if (type !== 'add') return;
     if (property && key !== property) return;
